1b1d919d67a4fe02a6f82d923170b425b703072d
MasterFlo-droid <77565066+MasterFlo-droid@users.noreply.github.com>
2021-03-15 18:16:43 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function toReadable (number) {
-  
-}
+  const dg = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  const tn = ['','','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+   if (number < 20) return dg[number]
+   const world = number % 10
+    if (number < 100) {
+    return tn[Math.floor(number / 10)] + (world ? ` ${dg[world]}` : `` )
+  }
+   return `${dg[Math.floor(number / 100)]} hundred${number % 100 !== 0 ? ` ${toReadable(number % 100)}` : `` }`
+}    
